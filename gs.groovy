37dd3829ee22444ddfa7e9128081e5104e16387c
hamdiz0 <hamdizaghouan21166@gmail.com>
2024-10-12 23:17:55 +0100

    updated

--- gs.groovy
+++ gs.groovy
@@ -2,7 +2,7 @@
 def build(String imageName ,String ver ,String credId ,String dockerfilelocation){
     withCredentials([
         usernamePassword(
-            credentialsId: "$credId",
+            credentialsId: "docker-repo",
             usernameVariable: "USER",
             passwordVariable: "PASSWORD"
         )
@@ -10,6 +10,7 @@ def build(String imageName ,String ver ,String credId ,String dockerfilelocation
         sh "docker build $dockerfilelocation -t $imageName:$ver"
         sh 'echo PASSWORD | docker login -u USER --password-stdin'
         sh "docker push $imageName:$ver"
+        echo "$credId"
     }
 }
 return this
