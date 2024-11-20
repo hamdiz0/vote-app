64851169cb070f5f774b0a2599ebcb2aad78a17f
hamdiz0 <hamdizaghouan21166@gmail.com>
2024-11-20 21:04:27 +0100

    updated gs.groovy

--- gs.groovy
+++ gs.groovy
@@ -13,15 +13,16 @@ def build(String imageName ,String version ,String credId ,String dockerfileloca
         sh "docker push $imageName:$version"
     }
 }
-// build(remote user name , remote Ip@ , credentialId from jenkins , github repo url , full script path : Repo-name/Path-to-scrip/Script-name )
-def deploy(String remote_user ,String remote_ip ,String version ,String credId ,String repo ,String script_path){
+// build(remote user name , remote Ip@ , credentialId from jenkins , github repo url , full script path : Repo-name/Path-to-scrip , Script-name )
+def deploy(String remote_user ,String remote_ip ,String version ,String credId ,String repo ,String script_path , String script_name){
     sshagent(credentials:["$credId"]){
         sh """
             ssh -o StrictHostKeyChecking=no $remote_user@$remote_ip <<EOF
                 rm -rf jenkins # remove old repofiles
                 mkdir jenkins ; cd jenkins ; git clone $repo # clone repo in jenkins folder
-                sudo chmod +x $script_path # add execute permissions
-                sh $script_path -v $version# run script
+                cd $script_path
+                sudo chmod +x $script_name # add execute permissions
+                sh $script_name -v $version# run script
         """
     }
 }
