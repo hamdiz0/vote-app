2d3060fdfcc3761314274e3525984039c268a0f2
hamdiz0 <hamdizaghouan21166@gmail.com>
2024-11-20 20:58:12 +0100

    testing cd

--- gs.groovy
+++ gs.groovy
@@ -1,6 +1,6 @@
 
 // build(DockerHub Profile/imageName , version , credentialId from jenkins , Dockerfile path)
-def build(String imageName ,String ver ,String credId ,String dockerfilelocation){
+def build(String imageName ,String version ,String credId ,String dockerfilelocation){
     withCredentials([
         usernamePassword(
             credentialsId: "$credId",
@@ -8,9 +8,22 @@ def build(String imageName ,String ver ,String credId ,String dockerfilelocation
             passwordVariable: "PASSWORD"
         )
     ]){
-        sh "docker build $dockerfilelocation -t $imageName:$ver"
+        sh "docker build $dockerfilelocation -t $imageName:$version"
         sh "echo $PASSWORD | docker login -u $USER --password-stdin"
-        sh "docker push $imageName:$ver"
+        sh "docker push $imageName:$version"
     }
 }
+// build(remote user name , remote Ip@ , credentialId from jenkins , github repo url , full script path : Repo-name/Path-to-scrip/Script-name )
+def deploy(String remote_user ,String remote_ip ,String version ,String credId ,String repo ,String script_path){
+    sshagent(credentials:["$credId"]){
+        sh """
+            ssh -o StrictHostKeyChecking=no $remote_user@$remote_ip <<EOF
+                rm -rf jenkins # remove old repofiles
+                mkdir jenkins ; cd jenkins ; git clone $repo # clone repo in jenkins folder
+                sudo chmod +x $script_path # add execute permissions
+                sh $script_path -v $version# run script
+        """
+    }
+}
+
 return this
--- k8s-specifications/k8s-cluster-run.sh
+++ k8s-specifications/k8s-cluster-run.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-echo "deleteing the old objects"
+# accept version with -v 
+while getopts "v:" opt; do
+  case $opt in
+    v) VER=$OPTARG;;
+  esac
+done
+
+echo "deleting the old objects"
 kubectl delete all --all
 
-DIR=${1:-.}
+DIR=. # set the directory to the default
 
 echo "applying k8s yaml files"
 
 for file in "$DIR"/*.y*ml; do
     if [ -f "$file" ]; then
         echo "applying $file ..."
+        sed -i "s/<<VERSION>>/${VER}/" "$file" # swap the <<VERSION>> in the YAML files with the passed version VER
         kubectl apply -f "$file"
     else
         echo "no yaml files in : $DIR"
@@ -21,7 +29,7 @@ echo -e "\nstarting pods ...\n"
 
 kubectl rollout status all
 
-kubectl rollout restart deployment/coredns --namespace=kube-system
+kubectl rollout restart deployment/coredns --namespace=kube-system # restart coredns to avoid dns issues (pods can't recognize service names)
 
 echo -e "\nkubectl get all :\n"
 kubectl get all
