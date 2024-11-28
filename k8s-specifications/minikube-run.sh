0a87506d9512f6e209123e048b0165f0cc8136a1
hamdi zaghouan <141347852+hamdiz0@users.noreply.github.com>
2024-11-28 23:48:04 +0100

    Update minikube-run.sh

--- k8s-specifications/minikube-run.sh
+++ k8s-specifications/minikube-run.sh
@@ -40,19 +40,17 @@ minikube service -- result-svc --url
 kill $(ps aux | grep 'kubectl port-forward' | awk '{print $2}')
 
 # port forwarding to access the service 
-minikube kubectl -- port-forward svc/vote-svc 30000:80 --address 0.0.0.0 > /dev/null 2>&1 &
+minikube kubectl -- port-forward svc/vote-svc 30000:80 --address 0.0.0.0 &  # & to run in the background
 # get port forwarding proccess id
 VOTE_PID=$!
 
-minikube kubectl -- port-forward svc/result-svc 30001:80 --address 0.0.0.0 > /dev/null 2>&1 &
+minikube kubectl -- port-forward svc/result-svc 30001:80 --address 0.0.0.0 &
 # get port forwarding proccess id
 RESULT_PID=$!
 
-# disown the processes so they won't terminate when the script ends
+# if you don't disown the forwarding process the script will stay in an execution state and the jenkins build wont end
+
 disown $VOTE_PID
 disown $RESULT_PID
 
-# if you don't disown the forwarding process the script will stay in an execution state and the jenkins build wont end
-
 echo "port forwarding started for vote-svc on 30000 and result-svc on 30001."
-
